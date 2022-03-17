76ae44c3f3059da1110df728e90844abd06a3551
Martín Termini <martin.termini@gmail.com>
2022-03-17 21:22:15 +0100

    add pokemon data detail component

---
+++ src/component/molecule/pokemon-data-detail/pokemon-data-detail.styled.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const StyledFeatureDiv = styled.div`
+  font-weight: bold;
+  margin: 3% 0;
+  width: 100%;
+`;
+
+export const StyledPreEvolutionDiv = styled.div`
+  display: flex;
+  margin-top: 10%;
+
+  div {
+    align-self: center;
+
+    span {
+      font-weight: bold;
+    }
+  }
+
+  img {
+    align-items: center;
+    align-self: center;
+    cursor: pointer;
+    height: 75px;
+  }
+`;
---
+++ src/component/molecule/pokemon-data-detail/pokemon-data-detail.tsx
@@ -0,0 +1,64 @@
+import { useNavigate } from 'react-router-dom';
+import { PokemonDetail } from '../../../data/type/pokemon';
+import { uppercaseFirstLetter } from '../../../data/util/string';
+import {
+  getPokemonIdFromDetailUrl,
+  getPokemonSpriteAssetUrlByPokemonId,
+} from '../../../data/util/url';
+import { Title } from '../../core/title/title';
+import {
+  StyledFeatureDiv,
+  StyledPreEvolutionDiv,
+} from './pokemon-data-detail.styled';
+
+type PokemonDetailDataProps = {
+  pokemonDetailedData: PokemonDetail;
+};
+
+const feature = ['Baby', 'Legendary', 'Mythical'];
+
+export const PokemonDataDetail = ({
+  pokemonDetailedData: {
+    description,
+    isBaby,
+    isLegendary,
+    isMythical,
+    previousForm,
+  },
+}: PokemonDetailDataProps) => {
+  const navigate = useNavigate();
+
+  const preEvolutionId =
+    // When a pre-evolution is from another generation, it's not the previous number
+    previousForm && getPokemonIdFromDetailUrl(previousForm.url);
+
+  const onPreEvolutionClick = () => navigate(`/details/${preEvolutionId}/`);
+
+  return (
+    <div>
+      <Title size="MEDIUM">Description</Title>
+      {description}
+
+      {[isBaby, isLegendary, isMythical].map((hasFeature, index) =>
+        hasFeature ? (
+          <StyledFeatureDiv key={feature[index]}>
+            - {feature[index]}
+          </StyledFeatureDiv>
+        ) : null
+      )}
+
+      {preEvolutionId && (
+        <StyledPreEvolutionDiv>
+          <div>
+            Evolves from: <span>{uppercaseFirstLetter(previousForm.name)}</span>
+          </div>
+          <img
+            src={getPokemonSpriteAssetUrlByPokemonId(preEvolutionId)}
+            onClick={onPreEvolutionClick}
+            alt={previousForm.name}
+          />
+        </StyledPreEvolutionDiv>
+      )}
+    </div>
+  );
+};

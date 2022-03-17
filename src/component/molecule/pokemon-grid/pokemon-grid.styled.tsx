b7f5f5c2fec717d1e164c89c6085d1ac6bd1225d
Martín Termini <martin.termini@gmail.com>
2022-03-17 21:21:08 +0100

    reorder grid component styles

--- src/component/molecule/pokemon-grid/pokemon-grid.styled.tsx
+++ src/component/molecule/pokemon-grid/pokemon-grid.styled.tsx
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
 export const StyledContainerDiv = styled.div`
-  width: 100%;
-  position: relative;
-
   display: grid;
   grid-template-columns: repeat(4, 1fr);
+
+  position: relative;
+  width: 100%;
 `;
